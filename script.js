9bba5a60eec6cc013bed269fb71a7d351ff9c3dd
TheMax370 <69474247+TheMax370@users.noreply.github.com>
2022-08-22 15:48:45 -0400

    Update script.js

--- script.js
+++ script.js
@@ -56,18 +56,64 @@ cardCVC.addEventListener('keyup', event => {
 button.addEventListener('click', event => {
     let name = cardHolderName.value;
     let nameREGEX = /^[a-zA-Z]+(([a-zA-Z ])?[a-zA-Z]*)*$/;
-    let result = nameREGEX.test(name);
-    
-    if (result === true) {
-        alert(result);
+    let resultName = nameREGEX.test(name);
+
+    if (resultName === false) {
+        document.getElementById('card-hn').style.borderColor = '#FF5050';
+        document.getElementById('error-name').innerHTML = 'Wrong format, letters only';
     }
 
-    else {
-        cardHolderName.style.borderColor = '#FF5050';
-        const textName = document.createElement('p');
-        textName.innerHTML = 'Wrong format, letters only'
-        document.getElementsByClassName('name-container')[0].appendChild(textName);
+    let number = cardNumber.value;
+    let cardNumRegex = /^((4\d{3})|(5[1-5]\d{2})|(6011)|(34\d{1})|(37\d{1}))-?\s?\d{4}-?\s?\d{4}-?\s?\d{4}|3[4,7][\d\s-]{15}$/;
+    let resultNum = cardNumRegex.test(number);
+
+    if (resultNum === false) {
+        document.getElementById('card-number').style.borderColor = '#FF5050';
+        document.getElementById('error-num').innerHTML = 'Wrong format, numbers only';
     }
-    
+
+    let monthNum = cardMonth.value;
+    let monthNumRegex = /0[1-9]|1[0-2]/;
+    let monthResult = monthNumRegex.test(monthNum);
+
+    if (monthResult === false) {
+        document.getElementById('month').style.borderColor = '#FF5050';
+        document.getElementById('error-date').innerHTML = 'Wrong format, MM only';
+    }
+
+    let yearNum = cardYear.value;
+    let yearRegex = /2[0-9]/;
+    let yearResult = yearRegex.test(yearNum);
+
+    if (yearResult === false) {
+        document.getElementById('month').style.borderColor = '#FF5050';
+        document.getElementById('error-date').innerHTML = 'Wrong format, YY only';
+    }
+
+    if (yearResult === false && monthResult === false) {
+        document.getElementById('year').style.borderColor = '#FF5050';
+        document.getElementById('month').style.borderColor = '#FF5050';
+        document.getElementById('error-date').innerHTML = 'Wrong format, YY only and MM only';
+    }
+
+    let cardCVCNum = cardCVC.value;
+    let cardCVCRegex = /^[0-9]{3,4}$/;
+    let cardCVCResult = cardCVCRegex.test(cardCVCNum);
+
+    if (cardCVCResult === false) {
+        document.getElementById('cvc').style.borderColor = '#FF5050';
+        document.getElementById('error-cvc').innerHTML = 'Wrong format, 3-4 digits only ';
+    }
+
+    if (resultName === true && resultNum === true && monthResult === true && yearResult === true && cardCVCResult === true) {
+        cardHolderName.style.display = 'none';
+        cardNumber.style.display = 'none';
+        cardMonth.style.display = 'none';
+        cardYear.style.display = 'none';
+        cardCVC.style.display = 'none';
+        
+    }
+
 })
+
 
